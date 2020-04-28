44d8da67a4f83697f9be2ef09028e87d48120518
andrunataliya <nataliya.andrukhiv@gmail.com>
2020-04-28 03:36:23 +0000

    add instance

---
+++ Apache Task1/ec2_instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0d6621c01e8c2de2c"
+  instance_type = "t2.large"
+  availability_zone = "us-west-2a"
+  key_name = "${aws_key_pair.us-west-2-key.key_name}"
+  security_groups = ["${aws_security_group.allow_tls.name}"]
+  user_data = "${file("install_apache.sh")}"
+#   device_name = "${aws_volume_attachment.ebs_att.device_name}"
+  tags = {
+    Name = "Task1"
+  }
+}
