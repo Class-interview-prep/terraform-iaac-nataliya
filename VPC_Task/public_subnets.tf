74a2b446750fdc7b104d23333d0700596bea3f18
andrunataliya <nataliya.andrukhiv@gmail.com>
2020-04-30 16:45:56 +0000

    update tags

--- VPC_Task/public_subnets.tf
+++ VPC_Task/public_subnets.tf
@@ -2,11 +2,27 @@
 resource "aws_subnet" "public_subnet1" {
     vpc_id     = "${aws_vpc.main.id}"
     cidr_block = "${var.pub_subnet1}"
+
+  tags = {
+    Environment = "${var.Environment}"
+    Department = "${var.Department}"
+    Team = "${var.Team}"
+    Created_by = "${var.Created_by}"
+  }
 }
+
 resource "aws_subnet" "public_subnet2" {
     vpc_id     = "${aws_vpc.main.id}"
     cidr_block = "${var.pub_subnet2}"
+
+  tags = {
+    Environment = "${var.Environment}"
+    Department = "${var.Department}"
+    Team = "${var.Team}"
+    Created_by = "${var.Created_by}"
+  }
 }
+
 resource "aws_subnet" "public_subnet3" {
     vpc_id     = "${aws_vpc.main.id}"
     cidr_block = "${var.pub_subnet3}"
